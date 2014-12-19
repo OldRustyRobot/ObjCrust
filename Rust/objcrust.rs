eb58a97a46073b6d286307efee3e6d9c2ee7676e
Valerii Hiora <valerii.hiora@gmail.com>
2014-12-19 20:03:54 +0200

    Updated example - since runtime removal there is no need anymore to create fake tasks and init runtime manually

--- Rust/objcrust.rs
+++ Rust/objcrust.rs
@@ -1,80 +1,19 @@
 #![crate_type = "staticlib"]
 
-extern crate native;
 extern crate libc;
 extern crate collections;
 
-use std::collections::hashmap::HashMap;
-use libc::c_char;
+use std::collections::HashMap;
 use std::comm::{channel, Sender};
 
 use std::iter;
-use std::rt;
-use std::rt::unwind::try;
-use std::rt::task::Task;
-use std::rt::local::Local;
-use std::c_str::CString;
 use std::time::Duration;
-use native::task;
 
-fn ignore_sigpipe() {
-    use libc;
-    use libc::funcs::posix01::signal::signal;
-    unsafe {
-        assert!(signal(libc::SIGPIPE, libc::SIG_IGN) != -1);
-    }
-}
-
-#[allow(dead_code)]
-fn run_proc_in_task(f: || -> ()) {
-    ignore_sigpipe();
-
-    rt::init(0, std::ptr::null());
-    let mut task = task::new((0, 0));
-    task.name = Some(std::str::Slice("<extra-task>"));
-    let t = task.run(|| {
-        f();
-    });
-    drop(t);
-}
-
-// Creates a "fake" task which will be presented always
-// It is required to have at least one task before
-// using std libs as most of them rely on correct
-// runtime initialization
-#[no_mangle]
-pub extern fn register_task(name: *const c_char) {
-    ignore_sigpipe();
-
-    rt::init(0, std::ptr::null());
-    let mut task = task::new((0, 0));
-    task.name = Some(std::str::Owned(unsafe {CString::new(name, false).as_str().unwrap().to_string()}));
-
-    Local::put(task);
-}
-
-#[no_mangle]
-pub extern fn deregister_task() {
-    let task: Box<Task> = Local::take();
-    drop(task);
-}
-
-#[no_mangle]
-pub extern fn run_rust_main() {
-    // Testing hack to get command line arguments into Rust
-    println!("Args are {}", std::os::args());
-    let _ = unsafe { try(|| { rust_main() }) };
-}
 
 pub static mut db_sender: *mut Sender<int> = 0 as *mut Sender<int>;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    /*
-    let ref mut w = std::io::stdout();
-    std::rt::backtrace::write(w);
-    */
-
     println!("Hello from Rust!");
 
     // Hashmap - testing rand module works
@@ -85,7 +24,7 @@ pub extern fn rust_main() {
     // Using channels
     let (tx, rx) = channel();
 
-    spawn(proc () {
+    spawn(move || {
         let (sender, receiver) = channel::<int>();
 
         // Testing if global variable will live
@@ -125,21 +64,20 @@ pub extern fn rust_main() {
 
     let (tx, rx) = channel();
     tx.send(200i);
-    spawn(proc() {
+    spawn(move || {
         let t = rx.recv();
         println!("Got {} from main thread", t);
-        fail!()
+        panic!()
     });
-    
-    // Testing exception handling and reporting
-    fail!();
 }
 
+#[deriving(Copy)]
 pub struct Pair {
     foo: uint,
     bar: uint,
 }
 
+#[deriving(Copy)]
 pub struct Complex {
     real: f64,
     img: f64,
