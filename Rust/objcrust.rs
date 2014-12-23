bcdf16d9e81e719282ef67c40487cacd762a3356
Valerii Hiora <valerii.hiora@gmail.com>
2014-12-23 20:01:24 +0200

    Testing arm64 on device only

--- Rust/objcrust.rs
+++ Rust/objcrust.rs
@@ -1,91 +1,30 @@
 #![crate_type = "staticlib"]
 
-extern crate std;
-extern crate native;
 extern crate libc;
 extern crate collections;
 
-use std::collections::hashmap::HashMap;
-use libc::c_char;
+use std::collections::HashMap;
 use std::comm::{channel, Sender};
 
 use std::iter;
-use std::rt;
-use std::rt::unwind::try;
-use std::rt::task::Task;
-use std::rt::local::Local;
-use std::c_str::CString;
-use native::task;
+use std::time::Duration;
 
-fn ignore_sigpipe() {
-    use libc;
-    use libc::funcs::posix01::signal::signal;
-    unsafe {
-        assert!(signal(libc::SIGPIPE, libc::SIG_IGN) != -1);
-    }
-}
 
-#[allow(dead_code)]
-fn run_proc_in_task(f: || -> ()) {
-    ignore_sigpipe();
-
-    rt::init(0, std::ptr::null());
-    let mut task = task::new((0, 0));
-    task.name = Some(std::str::Slice("<extra-task>"));
-    let t = task.run(|| {
-        f();
-    });
-    drop(t);
-}
-
-// Creates a "fake" task which will be presented always
-// It is required to have at least one task before
-// using std libs as most of them rely on correct
-// runtime initialization
-#[no_mangle]
-pub extern fn register_task(name: *c_char) {
-    ignore_sigpipe();
-
-    rt::init(0, std::ptr::null());
-    let mut task = task::new((0, 0));
-    task.name = Some(std::str::Owned(unsafe {CString::new(name, false).as_str().unwrap().to_string()}));
-
-    Local::put(task);
-}
-
-#[no_mangle]
-pub extern fn deregister_task() {
-    let task: Box<Task> = Local::take();
-    drop(task);
-}
-
-#[no_mangle]
-pub extern fn run_rust_main() {
-    // Testing hack to get command line arguments into Rust
-    println!("Args are {}", std::os::args());
-    let _ = unsafe { try(|| { rust_main() }) };
-}
-
-pub static mut db_sender: *Sender<int> = 0 as *Sender<int>;
+pub static mut db_sender: *mut Sender<int> = 0 as *mut Sender<int>;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    /*
-    let ref mut w = std::io::stdout();
-    std::rt::backtrace::write(w);
-    */
-
     println!("Hello from Rust!");
-    
+
     // Hashmap - testing rand module works
     let mut dict = HashMap::new();
-    dict.insert(3, 4);
-    dict.insert(4, 6);
-    
+    dict.insert(3i, 4i);
+    dict.insert(4i, 6i);
+
     // Using channels
     let (tx, rx) = channel();
 
-    spawn(proc () {
+    spawn(move || {
         let (sender, receiver) = channel::<int>();
 
         // Testing if global variable will live
@@ -98,49 +37,47 @@ pub extern fn rust_main() {
         tx.send(sender);
 
         println!("In daemon receiver");
-        std::io::timer::sleep(3000);
+        std::io::timer::sleep(Duration::seconds(3));
 
-        let mut z = 0;
+        let mut z = 0i;
 
         loop {
             let i = receiver.recv();
-            if i == 0 {
+            if i == 0i {
                 break;
             } else {
                 println!("Task got {} [{}]", i, z);
-                z += 1;
+                z += 1i;
             }
         }
 
         println!("Exiting daemon receiver");
     });
 
-
     let _ = rx.recv();
 
     unsafe {
-        for i in iter::range(1, 10) {
+        for i in iter::range(1i, 10) {
             (*db_sender).send(i);
         }
     }
 
     let (tx, rx) = channel();
-    tx.send(200);
-    spawn(proc() {
+    tx.send(200i);
+    spawn(move || {
         let t = rx.recv();
         println!("Got {} from main thread", t);
-        fail!()
+        panic!()
     });
-    
-    // Testing exception handling and reporting
-    fail!();
 }
 
+#[deriving(Copy)]
 pub struct Pair {
     foo: uint,
     bar: uint,
 }
 
+#[deriving(Copy)]
 pub struct Complex {
     real: f64,
     img: f64,
--- iOS/Sources/main.m
+++ iOS/Sources/main.m
@@ -1,23 +1,14 @@
 #import "CRAppDelegate.h"
 #import "objcrust.h"
+#import <execinfo.h>
 
-extern void run_rust_main();
-extern void register_task(const char *);
-extern void deregister_task();
+extern void rust_main();
 
 int main(int argc, char *argv[]) {
     // Enable printing backtraces
-    // It works correctly in Simulator, but doesn't work yet on the device
     setenv("RUST_BACKTRACE", "1", 1);
 
-    // Initially registering task which is required
-    // to get std libs working
-    register_task("<main>");
-    
-    // While it is possible to call rust_main directly
-    // it will crash the whole app as uses fail!()
-    // run_rust_main just wraps it into unwinder
-    run_rust_main();
+    rust_main();
     
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass(CRAppDelegate.class));
